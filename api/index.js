805c3607097815fe8e71283405f713f79e664033
ncjgestaoredes-del <ncjgestaoredes@gmail.com>
2026-01-14 10:07:32 +0200

    api
    Signed-off-by: ncjgestaoredes-del <ncjgestaoredes@gmail.com>

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// CONFIGURAÇÃO DO BANCO DE DADOS (AlwaysData)
+/*const db = mysql.createPool({
+  host: 'mysql-albertocossa.alwaysdata.net',
+  user: '430726',
+  password: 'Acossa@824018',
+  database: 'albertocossa_sistema',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});*/
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+
+
+
+
+
+
+db.connect(err => {
+  if (err) console.error("Erro MySQL:", err);
+  else console.log("MySQL conectado");
+});
+
+// cadastrar cliente
+app.post("/clientes", (req, res) => {
+  const { nome, email, telefone } = req.body;
+
+  const sql = "INSERT INTO clientes (nome, email, telefone) VALUES (?, ?, ?)";
+  db.query(sql, [nome, email, telefone], (err) => {
+    if (err) return res.status(500).json(err);
+    res.json({ message: "Cliente cadastrado com sucesso" });
+  });
+});
+
+// listar clientes
+app.get("/clientes", (req, res) => {
+  db.query("SELECT * FROM clientes", (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+});
+
+module.exports = app;
